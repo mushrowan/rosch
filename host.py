a6d6f86572f32bd6c1d290ad1dd1cbfecf1bb922
ro <roarch@proton.me>
2024-01-04 10:55:46 +0000

    some more stuff, reconsidering the scope of host.py

--- host.py
+++ host.py
@@ -3,34 +3,39 @@ import shlex
 
 
 class Host:
-    def __init__(self, alias, hostname, path_to_pem, port=22, user="root"):
+    def __init__(self, alias, hostname, path_to_pem, path_to_pub, port=22, user="root"):
         self.hostname = hostname
         self.alias = alias
         self.port = port
         self.user = user
-        self.path_to_pem = path_to_pem
+        self.path_to_pem = path_to_pem  # This will be shared between all hosts so maybe class attribute
+        self.path_to_pub = path_to_pub  # This will be shared between all hosts so maybe class attribute
 
-    def copyIDToServer(self, path_to_pem):
+    def addAliasToSSHConfig(self, path_to_config="$HOME/.ssh/config"):
         print(
-            f"Adding SSH key {path_to_pem} to {self.user}@{self.hostname} on port {self.port}"
+            "Appending new server config to ssh config..."
+        )  # It may be better to do this in the main function because this will continuously open and close the config
+        with open(path_to_config, "a") as ssh_config:
+            print(f"Checking if alias {self.alias} exists in {path_to_config}...")
+            print(
+                f"Appending SSH alias {self.alias} for {self.hostname} to local SSH config..."
+            )
+            ssh_config.write(
+                f"""Host {self.alias}
+            Hostname {self.hostname}
+            Port {self.port}
+            User {self.user}
+            Identityfile {self.path_to_pub}"""
+            )
+            # except FileNotFoundError: # Should we do this in the main function?
+            print("fuck")
+
+    def copyIDToServer(self):
+        print(
+            f"Adding SSH public key {self.path_to_pub} to {self.user}@{self.hostname} on port {self.port}"
         )
 
         args = shlex.split(
-            f"ssh-copy-id -i {path_to_pem} -p {self.port} {self.user}@{self.hostname}"
+            f"ssh-copy-id -i {self.path_to_pub} -p {self.port} {self.user}@{self.hostname}"
         )
         subprocess.run(args)
-
-    def addAliasToSSHConfig(self, path_to_config="$HOME/.ssh/config"):
-        print("Appending new server configs to ssh config...")
-        with open(path_to_config, "a") as ssh_config:
-            for host in host_list:
-                print(
-                    f"Appending SSH alias {host.alias} for {host.hostname} to local SSH config..."
-                )
-                ssh_config.write(
-                    f"""Host {host.alias}
-            Hostname {host.hostname}
-            Port {host.port}
-            User {host.user}
-            Identityfile {pem_path}"""
-                )
